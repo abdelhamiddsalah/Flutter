1cbab90b33d7dc7a3a04415c7a53663f9f369a15
abdelhamidsalah <abdelhamidsal@gmail.com>
2024-07-13 11:20:58 +0200

    first commit

---
+++ lib/Student.dart
@@ -0,0 +1,6 @@
+class delay{
+ static void getmethod()async{
+ await   Future.delayed(Duration(seconds: 4));
+    print("dataaaaaaaaaaaaaaaaaaaaaa");
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+
+import 'Student.dart';
+
+void main(){
+  print("data 1");
+  delay.getmethod();
+  print("data2");
+}
